676ab31194f2badca7187a03dfec93552da5650e
SeokyungKim <kskyung0624@gmail.com>
2023-11-28 20:43:46 +0900

    docs : 테스트 수행시 restdocs html 파일을 제대로 생성할 수 있도록 해결한다

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,90 +1,93 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.2.0"
-	id("io.spring.dependency-management") version "1.1.4"
-	id("org.asciidoctor.jvm.convert") version "3.3.2"
-	kotlin("jvm") version "1.9.20"
-	kotlin("plugin.spring") version "1.9.20"
-	kotlin("plugin.jpa") version "1.9.20"
+    id("org.springframework.boot") version "3.2.0"
+    id("io.spring.dependency-management") version "1.1.4"
+    id("org.asciidoctor.jvm.convert") version "3.3.2"
+    kotlin("jvm") version "1.9.20"
+    kotlin("plugin.spring") version "1.9.20"
+    kotlin("plugin.jpa") version "1.9.20"
 }
 
 group = "com.app.canyonfrs"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_17
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 val snippetsDir by extra { file("build/generated-snippets") }
 
 dependencies {
-	// web
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
+    // web
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
 
-	// data
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	runtimeOnly("com.h2database:h2")
-	runtimeOnly("com.mysql:mysql-connector-j")
+    // data
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("com.h2database:h2")
+    runtimeOnly("com.mysql:mysql-connector-j")
 
-	// security
-	implementation("org.springframework.boot:spring-boot-starter-security")
-	testImplementation("org.springframework.security:spring-security-test")
+    // security
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    testImplementation("org.springframework.security:spring-security-test")
 
-	// support
-	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+    // support
+    developmentOnly("org.springframework.boot:spring-boot-docker-compose")
 
-	// test
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    // test
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 
-	// docs
-	testImplementation("org.springframework.restdocs:spring-restdocs-asciidoctor")
-	testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+    // docs
+//    asciidoctor("org.springframework.restdocs:spring-restdocs-asciidoctor")
+    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs += "-Xjsr305=strict"
-		jvmTarget = "17"
-	}
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "17"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
 
-tasks.test {
-	outputs.dir(snippetsDir)
-}
+tasks {
+    // Test 결과를 snippet Directory에 출력
+    test {
+        outputs.dir(snippetsDir)
+    }
 
-tasks.asciidoctor {
-	dependsOn(tasks.test)
-	inputs.dir(snippetsDir)
+    asciidoctor {
+        // test 가 성공해야만, 아래 Task 실행
+        dependsOn(test)
 
-	doFirst {
-		delete {
-			file("src/main/resources/static/docs")
-		}
-	}
-}
+        // 기존에 존재하는 Docs 삭제(문서 최신화를 위해)
+        doFirst {
+            delete(file("src/main/resources/static/docs"))
+        }
 
-tasks.register("copyHTML", Copy::class) {
-	dependsOn(tasks.asciidoctor)
-	from(file("build/asciidoc/html5"))
-	into(file("src/main/resources/static/docs"))
-}
+        // snippet Directory 설정
+        inputs.dir(snippetsDir)
 
-tasks.build {
-	dependsOn(tasks.getByName("copyHTML"))
-}
+        // Ascii Doc 파일 생성
+        doLast {
+            copy {
+                from("build/docs/asciidoc")
+                into("src/main/resources/static/docs")
+            }
+        }
+    }
 
-tasks.bootJar {
-	dependsOn(tasks.asciidoctor)
-	dependsOn(tasks.getByName("copyHTML"))
+    build {
+        // Ascii Doc 파일 생성이 성공해야만, Build 진행
+        dependsOn(asciidoctor)
+    }
 }
