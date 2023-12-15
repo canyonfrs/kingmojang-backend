1b1e9baec0414f4ac4600a58a7f1123b1130fb70
JerryK026 <kskyung0624@gmail.com>
2023-12-15 16:32:37 +0900

    fix : MockMvc를 사용하므로 mock 환경에서 테스트할 수 있도록 RANDOMPORT 옵션을 제거한다

--- src/test/kotlin/com/app/canyonfrs/kingmojang/utils/AcceptanceTest.kt
+++ src/test/kotlin/com/app/canyonfrs/kingmojang/utils/AcceptanceTest.kt
@@ -3,7 +3,7 @@ package com.app.canyonfrs.kingmojang.utils
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.TestExecutionListeners
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
 @Retention(AnnotationRetention.RUNTIME)
 @TestExecutionListeners(value = [AcceptanceTestExecutionListener::class], mergeMode = TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS)
-annotation class AcceptanceTest()
+annotation class AcceptanceTest
