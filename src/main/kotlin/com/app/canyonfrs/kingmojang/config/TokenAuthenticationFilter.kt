6b3aa8603eb5a7896b702ee1e9e92071b9d9d91b
JerryK026 <kskyung0624@gmail.com>
2023-12-16 18:09:54 +0900

    fix : findByToken시 member가 null일 때 exception 발생하는 현상을 해결한다

--- src/main/kotlin/com/app/canyonfrs/kingmojang/config/TokenAuthenticationFilter.kt
+++ src/main/kotlin/com/app/canyonfrs/kingmojang/config/TokenAuthenticationFilter.kt
@@ -3,11 +3,14 @@ package com.app.canyonfrs.kingmojang.config
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.springframework.dao.EmptyResultDataAccessException
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
+import java.util.*
 
 @Component
 class TokenAuthenticationFilter(
@@ -29,12 +32,17 @@ class TokenAuthenticationFilter(
         val token = authHeader.substring(7)
 
         if (token != null && SecurityContextHolder.getContext().authentication == null) {
-            val userDetails = userDetailsService.loadUserByUsername(token) ?: null
+            var userDetails= userDetailsService.loadUserByUsername(token)
+
+            if (userDetails == null) {
+                filterChain.doFilter(request, response)
+                return
+            }
 
             val authToken = UsernamePasswordAuthenticationToken(
                 userDetails,
                 null,
-                userDetails?.authorities
+                userDetails.authorities
             )
 
             SecurityContextHolder.getContext().authentication = authToken
--- src/main/kotlin/com/app/canyonfrs/kingmojang/member/MemberRepository.kt
+++ src/main/kotlin/com/app/canyonfrs/kingmojang/member/MemberRepository.kt
@@ -3,5 +3,5 @@ package com.app.canyonfrs.kingmojang.member
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface MemberRepository : JpaRepository<Member, Long> {
-    fun findByToken(token: String): Member
+    fun findByToken(token: String): Member?
 }
